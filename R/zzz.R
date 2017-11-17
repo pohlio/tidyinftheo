8ff3b39ab4e07c3d32c855e92d60c24a937d0b48
Andy Pohl <andy@pohl.io>
2017-11-16 20:26:30 -0600

    Better loading of tidyverse packages upon loading of this lib.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,31 @@
+# abbreviated, from tidyverse/R/{attach,zzz}.R:
+
+core <- c("ggplot2", "tibble", "tidyr", "readr", "purrr", "dplyr", "stringr", "forcats")
+
+core_loaded <- function() {
+    search <- paste0("package:", core)
+    core[search %in% search()]
+}
+core_unloaded <- function() {
+    search <- paste0("package:", core)
+    core[!search %in% search()]
+}
+
+tidyverse_attach <- function() {
+    to_load <- core_unloaded()
+    if (length(to_load) == 0)
+        return(invisible())
+    lapply(to_load, library, character.only = TRUE, warn.conflicts = FALSE)
+    invisible()
+}
+
+is_attached <- function(x) {
+    paste0("package:", x) %in% search()
+}
+
 .onAttach <- function(...) {
-    requireNamespace('tidyverse')
+    needed <- core[!is_attached(core)]
+    if (length(needed) == 0)
+        return()
+    tidyverse_attach()
 }
